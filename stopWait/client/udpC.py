189f1e377887c5c0a7b098876d35147623dbd8c4
Germain <germain.v40@gmail.com>
2019-03-06 15:00:52 -0700

    transfer file

---
+++ stopWait/client/udpC.py
@@ -0,0 +1,38 @@
+#! /bin/python
+from socket import *
+
+# default params
+serverAddr = ('localhost', 50000)       
+
+import sys, re                          
+
+def usage():
+    print("usage: %s [--serverAddr host:port]"  % sys.argv[0])
+    sys.exit(1)
+
+try:
+    args = sys.argv[1:]
+    while args:
+        sw = args[0]; del args[0]
+        if sw == "--serverAddr":
+            addr, port = re.split(":", args[0]); del args[0]
+            serverAddr = (addr, int(port))
+        else:
+            print("unexpected parameter %s" % args[0])
+            usage()
+except:
+    usage()
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+file = 'data'
+clientSocket.sendto(file.encode(), serverAddr)
+
+f = open(file,"r")
+data = f.read(100)
+while (data):
+    if(clientSocket.sendto(data,serverAddr)):
+        print ("sending ...")
+        data = f.read(100)
+clientSocket.close()
+f.close()
---
+++ stopWait/server/udpS.py
@@ -0,0 +1,46 @@
+#! /bin/python
+from socket import *
+
+# default params
+serverAddr = ('', 50001)
+
+import sys
+def usage():
+    print("usage: %s [--serverPort <port>]"  % sys.argv[0])
+    sys.exit(1)
+
+try:
+    args = sys.argv[1:]
+    while args:
+        sw = args[0]; del args[0]
+        if sw == "--serverPort":
+            serverAddr = ("", int(args[0])); del args[0]
+        else:
+            print("unexpected parameter %s" % args[0])
+            usage()
+except:
+    usage()
+
+print("binding datagram socket to %s" % repr(serverAddr))
+
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(serverAddr)
+print("ready to receive")
+
+while 1:
+    data, clientAddrPort = serverSocket.recvfrom(2048)
+    print("from %s: rec'd '%s'" % (repr(clientAddrPort), data))
+    data.strip()
+    f = open(data.strip(),'wb')
+    data,addr = serverSocket.recvfrom(2048)
+
+try:
+    while(data):
+        f.write(data)
+        serverSocket.settimeout(2)
+        data,addr = serverSocket.recvfrom(2048)
+except timeout:
+    f.close()
+    serverSocket.close()
+    print ('File Downloaded')
+    
